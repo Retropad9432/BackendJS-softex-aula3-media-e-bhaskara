b20e91352733a92958dcdc085949cd7e8d3dcf1b
Retropad9432 <retropad987@gmail.com>
2025-08-12 16:31:56 -0300

    aula 3 com bhask incompleto e a media

---
+++ aula3bhaskara.js
@@ -0,0 +1,18 @@
+let a = 10
+let b = 20
+let c = 30
+
+let delta = ((b*b) - (4*a*c))
+
+console.log (delta)
+//essa parte de baixo ainda nao pega
+let bhask1 (-b+delta)/2*a
+
+let bhask2 (-b-delta)/2*a
+
+console. log (bhask1)
+console. log (bhask2)
+
+//o do professor ficou assim nao completo ainda
+
+let bhask1 (-b+Math.sqrt)/2*a
